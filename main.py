c729259c78eba725aae4c2622540d33a80b056f1
Erik Börve <81572776+BorveErik@users.noreply.github.com>
2022-02-12 13:13:24 +0100

    Cleanup commit

--- main.py
+++ main.py
@@ -5,162 +5,24 @@ from myThymio import myThymio
 from tdmclient import ClientAsync
 import sys
 import os
-import global_path as gp
 import time
 
 sys.path.insert(0, os.path.join(os.getcwd(), 'Vision'))
+sys.path.insert(0, os.path.join(os.getcwd(), 'Video'))
 sys.path.insert(0, os.path.join(os.getcwd(), 'GlobalNavTest'))
 sys.path.insert(0, os.path.join(os.getcwd(), 'Path_planning'))
 
 from Vision.visionMain import getNavigationMap
 from Vision.visionMain import doPerspectiveTransform
 from Vision.camera_pose import pose
+
+import Path_Planning.global_path as gp
+
 from ekf import *
+from local_obst_avoidance import *
+from Video.mainVideo import *
+from motion import *
 
-def obstacle_detect(node,thymio):
-    '''
-    Updates obstacle boolean in thymio object based on proximity sensors
-    input:
-        node object
-        thymio object
-    '''
-    prox = node.v.prox.horizontal[0:5]
-    sensorSum = prox[2] + prox[0] + prox[4]
-    if sensorSum > 0:
-        thymio.obstacleFound = True
-    else:
-        thymio.obstacleFound = False
-
-def localObstacleAvoidance(thymio):
-    '''
-    Returns motor target for local obstacle avoidance.
-    Based on proximity sensors and number of sample since last seen obstacle.
-
-    Input:
-        thymio object
-    '''
-    if thymio.obstacleFound:
-        thymio.setSpeed(-25,25)
-        thymio.avoidingObstacle = True
-    else:
-        if thymio.obstacle_iter < 3:
-            thymio.setSpeed(50,50)
-            thymio.obstacle_iter += 1
-        else:
-            thymio.setSpeed(35,-35)
-            thymio.obstacle_iter = 0
-
-            if thymio.smallestDist < 0.025:
-                        print('Going back to path!')
-                        thymio.setSpeed(0,0)
-                        thymio.avoidingObstacle = False
-                        thymio.current_point = thymio.closestPoint    # PERHAPS EDIT!!
-
-def kalman_measure(node,thymio):
-    '''
-    Calculates extended kalman filter pose estimations.
-    Based on pose estimation from camera and measurments of wheel speed
-    input:
-        node object
-        thymio object
-    '''
-    v_l, v_r = node.v.motor.left.speed , node.v.motor.right.speed     # Read motor speed
-    V_r, V_l = trans_vel(v_l), trans_vel(v_r)                         # velocity is translated to m/s
-    thymio.velocity_meas(calculate_vel(V_r,V_l))                      # velocity is written to thymio
-
-    u_l, u_r = node.v.motor.left.target, node.v.motor.right.target
-    U_l, U_r = trans_vel(u_l), trans_vel(u_r)
-
-    thymio.input_update(calculate_vel(U_r,U_l))
-
-    EKF(thymio)
-
-def moveThymio(thymio):
-    '''
-    Sets the wheel speed target in the thymio object
-    '''
-    move = 80
-    thymio.setSpeed(move,move)
-
-def rotateThymio(thymio):
-    '''
-    Rotates thymio towards angle between current and following point in global path.
-    Based on extended kalman filter pose estimate.
-    '''
-    rot = 20
-    if thymio.angle <= np.pi:
-        if thymio.measAngle > thymio.angle + np.pi or thymio.measAngle < thymio.angle:
-            thymio.setSpeed(-rot,rot)
-        else:
-            thymio.setSpeed(rot,-rot)
-    if thymio.angle > np.pi:
-        if thymio.measAngle > thymio.angle - np.pi and thymio.measAngle < thymio.angle:
-            thymio.setSpeed(-rot,rot)
-        else:
-            thymio.setSpeed(rot,-rot)
-
-def doControl(thymio):
-    '''
-    Swaps between moving forward and rotating towards following point in path.
-    Based on difference in distance and angle
-    '''
-    thymio.distance2path()
-    thymio.angle2point()
-
-    print('Angle of next point: ',thymio.angle)
-    print('Estimated angle: ',thymio.measAngle)
-    print('Point distance:',thymio.distPoint)
-
-    if abs(thymio.distPoint) < 0.03:        
-        rotateThymio(thymio)
-
-        if abs(thymio.anglePoint) < 0.1 or abs(thymio.anglePoint) > 2*np.pi-0.1:
-            thymio.setSpeed(0,0)
-            thymio.updatePoint()
-    else:
-        moveThymio(thymio)
-
-def makeVideo(thymio,frame_cropped):
-    '''
-    Plots path visuals in transformed frame
-    '''
-
-    # Plot optinal path
-    x_scale,y_scale =  gp.get_scale_factor_to_reality(frame_cropped)
-    for i in range(len(thymio.rx)):
-        plotPos = (int(thymio.rx[i] /x_scale * 1000),int(thymio.ry[i] / y_scale * 1000))
-        edt_frame = cv2.circle(frame_cropped,
-                    plotPos,
-                    radius=1,
-                    color = (0, 0, 255),
-                    thickness = 2)
-        if i == thymio.current_point:
-            edt_frame = cv2.rectangle(frame_cropped,
-                    (int(plotPos[0]-1),int(plotPos[1]-1)),
-                    (int(plotPos[0]+1),int(plotPos[1]+1)),
-                    color = (0, 255, 0),
-                    thickness = 3)
-
-    # Plot current position of thymio
-    plotPos = (int(thymio.pos_est[0][0] /x_scale * 1000),int(thymio.pos_est[0][1] / y_scale * 1000))
-    edt_frame = cv2.circle(frame_cropped,
-                    plotPos,
-                    radius = 7,
-                    color = (0,255, 0),
-                    thickness= 2)
-
-    # Plot all past position of the thymio
-    thymio.storePath(plotPos[0],plotPos[1])
-    for i in range(1,len(thymio.xPlot)):
-        plotPos = (int(thymio.xPlot[i]),int(thymio.yPlot[i]))
-        edt_frame = cv2.circle(frame_cropped,
-                    plotPos,
-                    radius = 1,
-                    color = (255,0 , 0),
-                    thickness= 2)
-
-    return edt_frame
-        
 
 with ClientAsync() as client:
     async def main():
